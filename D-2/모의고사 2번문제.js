03786e2013e1eba20f40ed37c5b1b25e0630d22d
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-03-15 22:11:19 +0900

    모의고사

--- D-2/모의고사 2번문제.js
+++ D-2/모의고사 2번문제.js
@@ -1,6 +1,7 @@
 function solution(month, day){
-    let a = new Date(`2022-${month}-${day}`)
+    let a = new Date(`2022-${month}-${day}`) //현재 날짜 값
     let result = new Date(a.setDate(a.getDate() + 98))
+    //현재 날짜 + 98 Date(일), Month(달), Year(년)
 	return result;
 }
 console.log(solution(1,18))
