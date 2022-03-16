139231065b9c28bdc340df04ca93ae7d028f3f56
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-03-16 20:20:21 +0900

    reduce accumulator

---
+++ D-3/reduce연습.js
@@ -0,0 +1,27 @@
+
+const numbers = [1,];
+
+const sum1 = numbers.reduce((accumulator, currentNumber) => {
+
+    accumulator + currentNumber});
+
+
+
+//accumuleator(callback함수의 반환값을 누적)
+//currentValue(배열의 현재 요소)
+// 모든 값을 더해서 나온다 sum1 = 55
+
+
+
+//accumulator  callback 함수의 반환값을 누적함
+
+
+
+acc cur idx 
+0   1   0 = 1
+1   2   1 = 3
+3   3   2 = 6
+6   4   3 = 10
+10  
+
+
