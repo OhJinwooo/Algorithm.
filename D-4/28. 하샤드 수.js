ab8750e4f00654ba290ddaf26ad3bd16e0b50fa8
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-03-17 22:55:48 +0900

    Algorithm

---
+++ D-4/28. 하샤드 수.js
@@ -0,0 +1,15 @@
+function solution(arr)
+{
+    var answer = [arr[0]]; //이 배열의 top 과 비교해서 같으면 안 넣고, 다르면 push
+    
+    for (var i=1; i < arr.length; i++) {
+        var top = answer[answer.length-1]; // 이 부분이 중요했다.  top 업데이트 
+        if (top !== arr[i]) {
+            answer.push(arr[i])
+        }
+    }
+    console.log(answer)
+    return answer;
+}
+
+solution(answer)
